609e80c6c365c74365c564565628e49a9f54c9ed
Pajenan.j <pajenan.j@gmail.com>
2025-03-08 04:41:30 +0700

    fix: adjust HeroSection styles to ensure full-screen background and improve component styling.

--- src/app/components/home/HeroSection.tsx
+++ src/app/components/home/HeroSection.tsx
@@ -1,28 +1,29 @@
 import ButtonMain from "../ui/ButtonMain";
 import CardItem from "../ui/CardItemFlashSale";
 
-
 export default function HeroSection() {
   return (
-    <main className="flex flex-col justify-center items-center bg-white">
-    <section 
-      className=" w-full  lg:h-full flex flex-col justify-center bg-contain   items-start  bg-no-repeat 
-      lg:bg-[url('/images/home-imgaes/mainsneaker.png')] 
-      bg-[url('/images/home-imgaes/main_mobile.avif')]"
-    >
-      {/* Content */}
-      <article className="pl-5 lg:pl-10 py-30 space-y-3 lg:space-y-5 lg:mt-[0rem]">
-        <h1 className="text-black drop-shadow-sm text-4xl font-bold lg:text-8xl  ">Step-up Your Stride</h1>
-        <div className="space-y-3 lg:space-y-5"> 
-        <p className="text-black text-xl ">Premium Athletic Footwear</p>
-          <ButtonMain type="primary">SHOW NOW</ButtonMain>
-        </div>
-      </article>
-{/* เหลือแก้ layout เล็กน้อย */}
-      <section className="w-full flex flex-row-reverse  pb-30 mt-14 lg:pb-40 pr-3.5 z-10   lg:pr-20">
-        <CardItem/>
+    <main className="relative flex flex-col justify-center items-center bg-white w-full overflow-x-hidden">
+      {/* Desktop Background */}
+      <div className="hidden lg:block absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center bg-[url('/images/home-imgaes/mainsneaker.png')]"></div>
+      {/* Mobile Background */}
+      <div className="lg:hidden absolute inset-0 bg-no-repeat bg-cover bg-center bg-[url('/images/home-imgaes/main_mobile.avif')]"></div>
+
+      <section className="relative w-full flex flex-col justify-center items-start min-h-screen">
+        {/* Content */}
+        <article className="pl-5 lg:pl-10 space-y-3 lg:space-y-5 lg:pt-12">
+          <h1 className="text-black drop-shadow-sm text-4xl font-bold lg:text-8xl">Step-up Your Stride</h1>
+          <div className="space-y-3 lg:space-y-5"> 
+            <p className="text-black text-xl">Premium Athletic Footwear</p>
+            <ButtonMain type="primary">SHOW NOW</ButtonMain>
+          </div>
+        </article>
+
+        {/* Card Item */}
+        <section className="w-full flex flex-row-reverse pt-[18rem]  z-10 lg:pr-20">
+          <CardItem />
+        </section>
       </section>
-      </section>
-      </main>
+    </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import * as motion from "motion/react-client"
 import HeroSection from "./components/home/HeroSection";
+import BestSellersSection from "./components/home/BestSellersSection";
 export default function Home() {
  
   return (
     <>   
-        <HeroSection/>
+      <HeroSection />
+      <BestSellersSection/>
     </>
   );
 }
