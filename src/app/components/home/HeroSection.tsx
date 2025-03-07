ca6fa45611ac096dffb92a53fbccee73a13f04e3
Pajenan.j <pajenan.j@gmail.com>
2025-03-07 19:11:43 +0700

    feat: add HeroSection component with responsive background and content.

--- src/app/components/home/HeroSection.tsx
+++ src/app/components/home/HeroSection.tsx
@@ -4,22 +4,25 @@ import CardItem from "../ui/CardItemFlashSale";
 
 export default function HeroSection() {
   return (
+    <main className="flex flex-col justify-center items-center bg-white">
     <section 
-      className=" w-full h-svh flex flex-col justify-center bg-contain   items-start  bg-no-repeat 
+      className=" w-full  lg:h-full flex flex-col justify-center bg-contain   items-start  bg-no-repeat 
       lg:bg-[url('/images/home-imgaes/mainsneaker.png')] 
       bg-[url('/images/home-imgaes/main_mobile.avif')]"
     >
       {/* Content */}
-      <article className="pl-5 py-30 w-full h-full space-y-3">
-        <h1 className="text-black drop-shadow-xl text-4xl font-bold">Step-up Your Stride</h1>
-        <p className="text-black text-xl">Premium Athletic Footwear</p>
-        <ButtonMain type="primary">SHOW NOW</ButtonMain>
+      <article className="pl-5 lg:pl-10 py-30 space-y-3 lg:space-y-5 lg:mt-[0rem]">
+        <h1 className="text-black drop-shadow-sm text-4xl font-bold lg:text-8xl  ">Step-up Your Stride</h1>
+        <div className="space-y-3 lg:space-y-5"> 
+        <p className="text-black text-xl ">Premium Athletic Footwear</p>
+          <ButtonMain type="primary">SHOW NOW</ButtonMain>
+        </div>
       </article>
-
-      <section className="w-full flex flex-row-reverse space-x-5  pb-52 pr-3.5 z-10  ">
-        <CardItem />
+{/* เหลือแก้ layout เล็กน้อย */}
+      <section className="w-full flex flex-row-reverse  pb-30 mt-14 lg:pb-40 pr-3.5 z-10   lg:pr-20">
+        <CardItem/>
       </section>
-      
-    </section>
+      </section>
+      </main>
   );
 }
--- src/app/components/ui/CardItemFlashSale.tsx
+++ src/app/components/ui/CardItemFlashSale.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import * as motion from "motion/react-client"
+import ButtonMain from "./ButtonMain";
 
 // เหลือวาง Logic  วนลูปแสดงข้อมูลสินค้าที่มีในระบบ
 
@@ -10,22 +11,29 @@ export default function CardItem() {
       initial={{ opacity: 0, y: 20 }} 
       animate={{ opacity: 1, y: 0 }} 
       transition={{ duration: 1, ease: "easeOut" }} 
-      className="w-64 h-60 bg-white/30 backdrop-blur-xl rounded-lg shadow-lg overflow-hidden z-50 "
+      className="w-60 h-64 bg-white/30 backdrop-blur-xl rounded-lg shadow-lg overflow-hidden z-50 lg:w-[35rem] lg:h-[18.5rem] lg:flex lg:flex-row "
       >
-      <figure className="flex flex-col items-center space-y-5 justify-center pt-10 "> 
+      <figure className="flex flex-col items-center space-y-5 justify-center pt-10   lg:justify-normal lg:p-5  "> 
 
-        <div className="w-52 h-28 bg-[#E9E6DD] backdrop-blur-sm rounded-lg "> 
+          <div className="relative w-52 h-28 bg-white backdrop-blur-sm rounded-lg  lg:w-80 lg:h-64"> 
+             <div className="absolute  pl-3 pr-3  z-10 text-center rounded bg-amber-400 mt-1 ml-2 "> 
+              <span className=" text-sm text-slate-700 "> SALE </span>
+              </div>
+            <img src="/images/home-imgaes/sneakerCard2.jpg" alt="sneaker" className="w-full h-full rounded-lg object-cover scale-75  lg:object-contain lg:object-center lg:scale-[80%] " />
+          </div>
           
-            <img src="/images/home-imgaes/sneakerCard.avif" alt="sneaker" className="w-full h-full rounded-lg object-cover scale-100 " />
-            
-        </div>
           </figure>   
 
-        <p className="pl-5 pt-5"> Product name  </p>
-        <div className="flex flex-row items-center space-x-3"> 
-        <p className="pl-5"> $140.00 </p> 
-        <del className="pl-5 text-gray-500">  $160.00 </del>
-        </div>
+        <figcaption className="pl-5 flex flex-col mt-5">   
+        <p className=" pt-5 font-bold"> Air Jordan 1 Low </p>
+        <div className="flex flex-row items-center pt-2 "> 
+        <p className=""> $140.00 </p> 
+        <del className=" text-gray-500 pl-2">  $160.00 </del>
+          </div>
+          <div className="mt-28 hidden lg:block"> 
+            <ButtonMain type="outline" >ADD TO CART</ButtonMain>
+            </div>
+        </figcaption>  
 
     </motion.div>
       
