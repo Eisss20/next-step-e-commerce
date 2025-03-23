9b25af885d32844dd0070da9851714cac3c75494
Pajenan.j <pajenan.j@gmail.com>
2025-03-24 00:09:44 +0700

    fix: location file component

--- src/app/components/home/ShopCollection.tsx
+++ src/app/components/home/ShopCollection.tsx
@@ -1,20 +1,18 @@
-import ButtonMain from "../ui/ButtonMain";
-import CardBestSeller from "../products/CardBestSeller";
+import ButtonMain from '../ui/ButtonMain';
+import CardBestSeller from '../ui/CardBestSeller';
 
 export default function ShopCollection() {
-  
-  
   return (
-    <> 
-    <div className="overflow-hidden container mx-auto">
-    <section className="container min-h-screen   w-full mt-10 pl-5 pr-5 lg:pl-10 lg:pr-10 lg:w-[100vw] mx-auto">
-    <article  className="flex justify-between item-center   ">  
-          <h1 className=" flex  items-center text-xl font-medium  "> BEST SELLERS </h1>
-                 <ButtonMain type="primary">SHOW NOW</ButtonMain>
-    </article>
-    <CardBestSeller />  
-      </section>
-    </div>
-      </>
+    <>
+      <div className="container mx-auto overflow-hidden">
+        <section className="container mx-auto mt-10 min-h-screen w-full pr-5 pl-5 lg:w-[100vw] lg:pr-10 lg:pl-10">
+          <article className="item-center flex justify-between">
+            <h1 className="flex items-center text-xl font-medium"> BEST SELLERS </h1>
+            <ButtonMain type="primary">SHOW NOW</ButtonMain>
+          </article>
+          <CardBestSeller />
+        </section>
+      </div>
+    </>
   );
 }
