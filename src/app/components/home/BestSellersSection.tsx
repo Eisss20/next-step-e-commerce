1bcf0d0cbd87a192b455baeb7c4f17bf54e00b77
Pajenan.j <pajenan.j@gmail.com>
2025-04-11 04:48:42 +0700

    stlye: improve respondsive for landing page

--- src/app/components/home/BestSellersSection.tsx
+++ src/app/components/home/BestSellersSection.tsx
@@ -4,11 +4,11 @@ import CardBestSeller from '../ui/CardBestSeller';
 export default function BestSellersSection() {
   return (
     <>
-      <div className="container mx-auto overflow-hidden ">
+      <div className="container mx-auto overflow-hidden pb-10 lg:pb-0 ">
         <section className="container mx-auto mt-10 min-h-screen w-full pr-5 pl-5 lg:w-[100vw] lg:pr-10 lg:pl-10">
           <article className="item-center flex justify-between">
             <h1 className="flex items-center text-xl font-medium"> BEST SELLERS </h1>
-            <ButtonMain type="primary" onClick={() => {}}>SHOW NOW</ButtonMain>
+            <ButtonMain type="primary" onClick={() => {}}>SHOP NOW</ButtonMain>
           </article>
           <CardBestSeller/>
         </section>
--- src/app/components/home/ShopCollection.tsx
+++ src/app/components/home/ShopCollection.tsx
@@ -4,11 +4,11 @@ import CardCollection from '../ui/CardCollection';
 export default function ShopCollection() {
   return (
     <>
-      <div className="container mx-auto overflow-hidden ">
+      <div className="container mx-auto overflow-hidden pb-10 lg:pb-0">
         <section className="container mx-auto mt-10 min-h-screen w-full pr-5 pl-5 lg:w-[100vw] lg:pr-10 lg:pl-10">
           <article className="item-center flex justify-between">
             <h1 className="flex items-center text-xl font-medium "> COLLECTION </h1>
-            <ButtonMain type="primary">SHOW NOW</ButtonMain>
+            <ButtonMain type="primary">SHOP NOW</ButtonMain>
           </article>
           <CardCollection />
         </section>
